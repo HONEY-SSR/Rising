ae327deba4df2e86ca192ed8dde617db3f2d7d68
SSR <88606535+HONEY-SSR@users.noreply.github.com>
2022-06-05 16:03:52 +0800

    Initial Commit

---
+++ Rising/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Rising
+//
+//  Created by SSR on 2022/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Rising/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Rising
+//
+//  Created by SSR on 2022/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
