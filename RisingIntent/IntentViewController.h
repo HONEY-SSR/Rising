db8f49ae2812e9632baeb581f43042f404afd1df
SSR <88606535+HONEY-SSR@users.noreply.github.com>
2022-06-07 16:00:30 +0800

    [feature]假更新

---
+++ RisingIntent/IntentViewController.h
@@ -0,0 +1,12 @@
+//
+//  IntentViewController.h
+//  RisingIntent
+//
+//  Created by SSR on 2022/6/6.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
